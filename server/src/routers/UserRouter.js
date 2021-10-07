93e9e6c02ff77d6ff3d219e108622bc79caea501
Runabox <murrellcooper@gmail.com>
2021-10-06 20:38:33 -0400

    Return secret key in any case

--- server/src/routers/UserRouter.js
+++ server/src/routers/UserRouter.js
@@ -614,6 +614,7 @@ class UserRouter {
                                 success: true,
                                 username: user.username,
                                 key: user.uploadKey,
+                                secretKey: user.secretKey,
                                 pfp: user.pfp
                             });
 
